ea73ea46ffde3320692d569241463b5536d57276
ardinur03 <ardinurinsan03@gmail.com>
2020-04-13 10:36:04 +0700

    tambah nama identitas

--- belajar.py
+++ belajar.py
@@ -1,1 +1,7 @@
-print("ini dari teks editor")
+print("\t WELCOME TO LEARN PYTHON")
+
+# identitas about me
+Nama  = "Muhamad Ardi Nur Insan"
+Kelas = "11 RPL 1"
+print("Nama  :", Nama +"\nKelas :",Kelas)
+print("--------------------------------------")
