fd9480e0593fc2f438d465c483a6f48e8213296c
ardinur03 <ardinurinsan03@gmail.com>
2020-04-13 15:03:45 +0700

    isi

--- belajar.py
+++ belajar.py
@@ -1,7 +1,70 @@
 print("\t WELCOME TO LEARN PYTHON")
 
 # identitas about me
+"""
 Nama  = "Muhamad Ardi Nur Insan"
 Kelas = "11 RPL 1"
-print("Nama  :", Nama +"\nKelas :",Kelas)
-print("--------------------------------------")
+print("Nama  :", Nama +"\nKelas :", Kelas)
+print("----------------\"-----\"-----------------")
+"""
+
+
+# aplikasi sederhana penanyaan Nama
+# concatinetion (penggabungan string)
+"""
+nama = input("\nSiapa nama anda bosqu?? ")
+umur = input("\nUmur berapa anda?? ")
+print("Wihh..salam kenal! boss " + nama + " Umur anda  " + umur + " Tahun")
+"""
+
+
+# boolean
+"""
+x = "ardi"
+print(x.isalnum()) #mengecek apakah ini nomber atau bukan
+"""
+
+
+#if else
+"""
+angka1 = 90
+angka2 = 100
+if angka1 > angka2:
+    print("Horee benar!!!")
+else:
+    print("Oow! salah!")
+"""
+
+# aplikasi sederhana hitung hutang if-elif-else
+"""
+uang = input("Uang anda berapa? ")
+hutang = 10000
+
+if int(uang) < hutang:
+    print("Hutang anda : ", hutang)
+    print("Uangnya kurang bos!!!:)")
+elif int(uang) == hutang:
+    print("Hutang anda : ", hutang)
+    print("Terimakasih sudah membayar :)")
+else:
+    hasil = int(uang)-hutang
+    print("Hutang anda : ", hutang)
+    print("Uang lebih bos!!! Kembaliannya : ", hasil)
+"""
+
+
+# contoh if else bercabang
+"""
+Putri raja ingin menikah dengan syarat baik dan rajin
+"""
+tamu  = "cwe"
+baik  = True
+rajin = True
+
+if baik & rajin:
+    if tamu == "cwo":
+        print("Nikah yuk!!!")
+    else:
+        print('kita sodaraan saja!!!')
+else:
+    print("hush! sana!!!")
