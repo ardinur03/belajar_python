8c23a0872612dde75eb855f95065d46f161b208e
ardinur03 <ardinurinsan03@gmail.com>
2020-04-14 15:41:59 +0700

    isi

---
+++ air_suhu.py
@@ -0,0 +1,7 @@
+suhu = float(input())
+if int(suhu) <= 0:
+    print("Beku!!!")
+elif int(suhu) > 0 and int(suhu) <= 100:
+    print("Cair")
+else:
+    print("Uap!!!")
---
+++ perulangan.py
@@ -0,0 +1,20 @@
+# for
+"""
+total= 0
+for x in range(1, 6):
+    print ("Masukkan bilangan ke", x)
+    total = total + int(input())
+total = total / 5
+print("Rata-rata : ", total)
+"""
+
+# while
+n = int(input("Anda ingin menghitung berapa bilangan? "))
+total = 0
+counter =1
+while(counter <= n):
+    print("Masukkan bilangan ke", counter)
+    total = total + int(input())
+    counter = counter+1
+total = total / n
+print("Rata-rata : ", total)
