1f85b38dd232a142caeae625acc00eddaf4a0382
ardinur03 <ardinurinsan03@gmail.com>
2020-04-14 09:55:13 +0700

    isi

--- belajar.py
+++ belajar.py
@@ -80,7 +80,20 @@ while hitung < 5:
 """
 
 # for in
+"""
 orang = ['Ardi', 'Ahmad', 'Bujank', 'Naufal', 'Diaz']
 #judul = "YT Ardi Hydra"
 for nama in orang:
     print("Nama orangnya adalah : ", nama)
+"""
+
+# aplikasi sederhana persegi panjang
+print("Masukkan panjang : ")
+panjang = float(input())
+if (panjang < 0):
+    panjang = panjang * -1
+lebar   = float(input("Masukkan lebar : "))
+if (lebar  < 0):
+    lebar = lebar * -1
+luas = panjang * lebar
+print("Luas segiempat : ", luas)
