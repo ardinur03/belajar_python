339bfc624e3f54124d47936dfa846edbb6fbbc30
ardinur03 <ardinurinsan03@gmail.com>
2020-04-15 16:29:30 +0700

    isi

---
+++ module/app.py
@@ -0,0 +1,9 @@
+# import data as d         -> untuk menimport seluruh data
+# from data import nama    -> untuk mengimport hanya bagian objek tertentu
+
+import data as d
+
+
+print(d.person)
+print(d.nama("Jack"))
+print(d.benda)
---
+++ module/data.py
@@ -0,0 +1,14 @@
+person = {
+    'Nama' : "Muhamad Ardi Nur Insan",
+    'Umur' : "17 tahun",
+    'Kelas' : "XI RPL 1"
+}
+benda = [
+    'hp',
+    'buku',
+    'pulpen',
+    'pensil'
+]
+
+def nama(Nama = "Parameter nama belum di isi"):
+    print("Nama nya adalah : ",Nama)
